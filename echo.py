11275ea130632127c2d29d5d6781c2fa6445c641
Janell Huyck <Janell.Huyck@outlook.com>
2020-03-05 01:56:33 -0500

    test for -ult not working yet, but the other tests and the program itself are working.

--- echo.py
+++ echo.py
@@ -2,21 +2,58 @@
 # -*- coding: utf-8 -*-
 """An enhanced version of the 'echo' cmd line utility"""
 
-__author__ = "???"
+__author__ = """Janell.Huyck with help from madarp, knmarvel, and:
+                https://stackoverflow.com/questions/
+                    18160078/how-do-you-write-tests-for-the-argparse-
+                    portion-of-a-python-module/55234595#55234595
+                https://stackoverflow.com/questions/4042452/display-
+                    help-message-with-python-argparse-when-script-is-
+                    called-without-any-argu"""
 
 
 import sys
+import argparse
+
+if sys.version_info[0] >= 3:
+    raise Exception("This program requires python2 interpreter")
 
 
-def create_parser():
+def parse_args(sys_args):
     """Creates and returns an argparse cmd line option parser"""
-    pass
+
+    parser = argparse.ArgumentParser(add_help=True,
+                                     description="""Perform transformation
+                                     on input text.""")
+    parser.add_argument('text', help='text to be manipulated')
+    parser.add_argument('-u', '--upper', action="store_true",
+                        help='convert text to uppercase')
+    parser.add_argument('-l', '--lower', action="store_true",
+                        help='convert text to lowercase')
+    parser.add_argument('-t', '--title', action="store_true",
+                        help='convert text to titlecase')
+
+    if len(sys.argv) == 1:
+        print("\nCaught with too few arguments. How embarrassing...\n")
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+
+    parsed_args = parser.parse_args(sys_args)
+
+    return parsed_args
 
 
-def main(args):
+def main():
     """Implementation of echo"""
-    pass
+    args = parse_args(sys.argv[1:])
+    if args.upper:
+        print(args.text.upper())
+    if args.lower:
+        print(args.text.lower())
+    if args.title:
+        print(args.text.title())
+    if not args.upper and not args.lower and not args.title:
+        print(args.text)
 
 
 if __name__ == '__main__':
-    pass
+    main()
--- tests/test_echo.py
+++ tests/test_echo.py
@@ -1,12 +1,59 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import unittest
-import echo
+import subprocess
+
 
 # Your test case class goes here
+
+
 class TestEcho(unittest.TestCase):
-    pass
+    def test_no_manipulation(self):
+
+        process = subprocess.Popen(
+            ["python", "./echo.py", "hEllo"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+
+        self.assertEquals(stdout, "hEllo\n")
+
+    def test_uppercase_manipulation(self):
+
+        process = subprocess.Popen(
+            ["python", "./echo.py", "hEllo", "--upper"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+
+        self.assertEquals(stdout, "HELLO\n")
+
+    def test_lowercase_manipulation(self):
+
+        process = subprocess.Popen(
+            ["python", "./echo.py", "hEllo", "--lower"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+
+        self.assertEquals(stdout, "hello\n")
+
+    def test_titlecase_manipulation(self):
+
+        process = subprocess.Popen(
+            ["python", "./echo.py", "hEllo", "-t"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+
+        self.assertEquals(stdout, "Hello\n")
+
+    def test_all_case_manipulation(self):
+
+        process = subprocess.Popen(
+            ["python", "./echo.py", "hEllo", "-ulf"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+
+        self.assertEquals(stdout, "HELLO\nhello\nHello\n")
+
 
 if __name__ == '__main__':
     unittest.main()
